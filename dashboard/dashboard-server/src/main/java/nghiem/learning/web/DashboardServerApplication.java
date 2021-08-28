1a494c44cb9731fa7fd5610c677f36b39f41c30d
Bui Van Nghiem <37972011+bvnghiem@users.noreply.github.com>
2021-08-28 16:42:07 +0700

    dashboard server project init

---
+++ dashboard/dashboard-server/src/main/java/nghiem/learning/web/DashboardServerApplication.java
@@ -0,0 +1,13 @@
+package nghiem.learning.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardServerApplication.class, args);
+	}
+
+}
---
+++ dashboard/dashboard-server/src/test/java/nghiem/learning/web/DashboardServerApplicationTests.java
@@ -0,0 +1,13 @@
+package nghiem.learning.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
